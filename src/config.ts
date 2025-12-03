8589aa1b5b510c617540891afab41ae0effcb884
Lucas <duongquochuy190607@gmail.com>
2025-12-03 09:03:57 +0700

    Update bio formatting in profileConfig

--- src/config.ts
+++ src/config.ts
@@ -55,7 +55,7 @@ export const navBarConfig: NavBarConfig = {
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Quoc Huy",
-	bio: "Undergrad @ VNU \n Data Scientist",
+	bio: "Undergrad @ VNU <br /> Data Scientist",
 	links: [
 		{
 			name: "Twitter",
